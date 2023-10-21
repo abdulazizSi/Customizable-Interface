eca1f21beb64a58d8da0a84a882669d069305a0c
abdulazizSi <abdulaziz.si.aa@gmail.com>
2023-10-21 16:09:19 +0300

    Initial Commit

---
+++ Customizable Interface/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Customizable Interface
+//
+//  Created by Abdulaziz Alsikh on 21.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
