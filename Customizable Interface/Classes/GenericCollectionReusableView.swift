e6873c73145ec5f8ff7eb009b806dd837679dd18
abdulazizSi <abdulaziz.si.aa@gmail.com>
2023-10-22 14:06:07 +0300

    created generic collection reusable view

--- Customizable Interface/Classes/GenericCollectionReusableView.swift
+++ Customizable Interface/Classes/GenericCollectionReusableView.swift
@@ -5,4 +5,23 @@
 //  Created by Abdulaziz Alsikh on 22.10.2023.
 //
 
-import Foundation
+import UIKit
+
+class CollectionReusableView<MainView: UIView>: UICollectionReusableView {
+   
+    let mainView = MainView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(mainView)
+        mainView.fillToSuperview(.fullSpace())
+    }
+    
+    @available(*, unavailable, message: "Nibs are unsupported")
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+        
+}
+
+
